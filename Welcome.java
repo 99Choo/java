9b86f3cc1bbb07393ecb808e71a96cb413da7214
99Choo <127376295+99Choo@users.noreply.github.com>
2024-09-26 13:58:52 +0900

    Delete Welcome.java

--- Welcome.java
+++
@@ -1,8 +0,0 @@
-package OpenChallenge1;
-
-public class Welcome {
-	public static void main(String[] args) {
-		System.out.println("자바는 처음이죠?");
-		System.out.println("Welcome Java World~~");
-	}
-}
