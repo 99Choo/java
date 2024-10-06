42f88415f0a751fae4e8f7924602f9662eb1840d
99Choo <127376295+99Choo@users.noreply.github.com>
2024-10-06 17:38:42 +0900

    Delete Home_Work directory

--- Home_Work/P52_1.java
+++
@@ -1,9 +0,0 @@
-package Home_Work;
-//OpenChallenge
-public class P52_1 {
-	public static void main(String[] args) {
-		System.out.println("Choo sang hun");
-		System.out.println("26 years old");
-		System.out.print("Department of Computer Engineering");
-	}
-}
