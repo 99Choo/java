6fa4f962fe364213c577f55b060e832cd5fd8c15
99Choo <127376295+99Choo@users.noreply.github.com>
2024-12-01 16:09:56 +0900

    Delete 0916 directory

--- 0916/MyInfoApp.java
+++
@@ -1,9 +0,0 @@
-package OpenChallenge1;
-
-public class MyInfoApp {
-	public static void main(String[] args) {
-		System.out.println("Choo Sang Hun");
-		System.out.println("20 years old");
-		System.out.println("Department of Computer Engineering");
-	}
-}
